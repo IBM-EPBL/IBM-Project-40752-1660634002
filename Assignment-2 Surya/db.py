47815cbf61bdfd8a902efcb660a16bfcb003923e
Aswin-Web <109794592+Aswin-Web@users.noreply.github.com>
2022-10-02 18:53:51 +0530

    Delete Assignment-2 Surya directory

--- Assignment-2 Surya/db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-con=sqlite3.connect("items.db")
-print("successfullly connected");
-
-con.execute("CREATE TABLE items (name TEXT,email TEXT,phone TEXT ,password TEXT)")
-
-print("table created")
-con.close()
